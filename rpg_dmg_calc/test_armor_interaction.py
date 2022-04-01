a049c4e1d92cf9c105e6a45a436518d9717de1d6
TribQq <ghhghdffgj@gmail.com>
2022-04-01 22:59:19 +0500

    tests upd

--- rpg_dmg_calc/test_armor_interaction.py
+++ rpg_dmg_calc/test_armor_interaction.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
-
 import pytest
-
 from armor_interaction import PredefinedArmorDb, DamageCalculator, ArmorLayer
-
-
 @pytest.fixture
 def armor_db() -> PredefinedArmorDb:
     db = PredefinedArmorDb()
     db.load_json()
     return db
-
-
 @pytest.fixture
 def damage_calculator() -> DamageCalculator:
     return DamageCalculator()
-
-
 def test_get_armor_layers(armor_db: PredefinedArmorDb):
     assert (
         armor_db.get_armor_layers(
@@ -29,8 +21,6 @@ def test_get_armor_layers(armor_db: PredefinedArmorDb):
         )
         == [ArmorLayer("H", 2), ArmorLayer("M", 1), ArmorLayer("Ls", 2)]
     )
-    
-    
 def test_get_damage(damage_calculator: DamageCalculator):
     assert (
         damage_calculator.get_damage(
@@ -41,3 +31,18 @@ def test_get_damage(damage_calculator: DamageCalculator):
         )[0]
         == 11
     )
+    assert (
+        damage_calculator.get_damage(
+            10,
+            "p",
+            4,
+            [ArmorLayer("H", 2)],
+        )[0]
+        == 8
+    )
+
+
+def test_get_damage_no_armor(damage_calculator: DamageCalculator):
+    assert damage_calculator.get_damage(15, "p", 8, [])[0] == 15
+    assert damage_calculator.get_damage(15, "p", 0, [])[0] == 15
+    assert damage_calculator.get_damage(15, "x", 0, [])[0] == 15
